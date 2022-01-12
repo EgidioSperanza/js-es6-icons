046675c847de748fb190501b1abc195177911d9d
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-01-12 14:57:02 +0100

    select funzionante

--- main.js
+++ main.js
@@ -114,6 +114,10 @@ const icons = [
 	}
 ];
 
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+};
+
 function boxTemplate(container, icon, name){
     const boxIcon = document.createElement("div");
     boxIcon.className=("box-icon");
@@ -123,14 +127,63 @@ function boxTemplate(container, icon, name){
         <div>${displayIcon}</div>
         <div>${name}</div>
     `
+};
+
+function createSelect(){
+    const navSelect = document.querySelector(".filter-nav");
+
+    const iconsTypeSelect=`
+    <label>FILTRA PER TIPO:
+    <select class="filter-icons" name="filter-icons">
+    <option value="all">All</option>`
+    +
+    createOption(icons);
+    +
+    `</select>
+    </label>
+    <div class="result"></div>
+    `
+    navSelect.innerHTML=iconsTypeSelect;
+    const selectElement = document.querySelector('.filter-nav');
+};
+
+function createOption(e){
+    let options="";
+    const arrayTypeIcons = e.map(function(e){
+        return e.type;
+    });
+    // console.log(arrayTypeIcons)
+
+    const filteredTypeIcons = arrayTypeIcons.filter(function(type, i){
+        return arrayTypeIcons.indexOf(type) == i;    
+    });
+    // console.log(filteredTypeIcons)
+    filteredTypeIcons.forEach((e) => {
+        options += `<option value="${e}">${capitalize(e)}</option>`
+    });
+    // console.log(options);
+    return options
+};
+
+function chooseSelect(){
+    const selectElement = document.querySelector('.filter-icons');
+    selectElement.addEventListener('change', (event) => {
+        grid.innerHTML="";
+        icons.forEach((icons) => {
+            if (icons.type === event.target.value || event.target.value==="all"){
+                boxTemplate(grid, icons, icons.name.toUpperCase())
+            }
+        });
+    
+      });
 }
 
 function displayGrid(){
+    createSelect();
+    chooseSelect();
     icons.forEach((icons) => {
-        boxTemplate(grid, icons, icons.name)
+        boxTemplate(grid, icons, icons.name.toUpperCase())
     });
-}
+};
 
 displayGrid();
-
-
